9bc665fe30c03335a3168a91be243336cd73ecc9
jtimberman <joshua@opscode.com>
2012-11-18 14:31:32 -0700

    release v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs 1password"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.2"
 supports         "mac_os_x"
