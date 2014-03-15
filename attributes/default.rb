17ddfbf92298336cb01c5307739311fb0aab29c2
Julian C. Dunn <jdunn@aquezada.com>
2014-03-15 18:37:10 -0400

    Upgrade to 4.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['one_password']['version'] = '4.1.3'
-default['one_password']['checksum'] = '2e1ed8f13c3b86e7c7f226ba4a1bd274adbad636472375871637d2f1b447b58c'
+default['one_password']['version'] = '4.2.2'
+default['one_password']['checksum'] = 'f472d12c879cec5620d22ec805e74d881cd6b41f2c254e960ce5d8ecb525dc6f'
