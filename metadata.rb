836280d2fd54a3678d0694521ddd451434808d23
Brian Hays <brian.hays@gmail.com>
2015-03-02 20:46:11 -0500

    revert metadata.rb and CHANGELOG.md changes

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs 1password"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "1.1.1"
 supports         "mac_os_x"
