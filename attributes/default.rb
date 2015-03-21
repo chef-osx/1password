311b2045908d0aa9a572f6f899917987f34dd5f7
Julian C. Dunn <jdunn@aquezada.com>
2015-03-21 13:08:42 -0400

    Fixed missing .to_i

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-if node['platform_version'].split(".")[1] < 10
+if node['platform_version'].split(".")[1].to_i < 10
   default['one_password']['version'] = '4.4.3'
   default['one_password']['checksum'] = '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
   default['one_password']['appname'] = '1Password 4.app'
